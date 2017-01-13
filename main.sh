1a4d45b1b271e83c480c0c38e01c6ff75c7f96ec
Hassan Mohammed <hassanmamin994@gmail.com>
2017-01-13 18:44:22 +0200

    Create multiple databases added

--- main.sh
+++ main.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+##################################################
+# Functions that creating tables
+##################################################
 function createTable {
+	unset 'columnNames';
+	unset 'columnType' ;
 	echo "Please Enter Table's name ";
 	read tableName ;
-	if [ -f "tableinfo/$tableName" ]
+	if [ -f "$1/tableinfo/$tableName" ]
 	then
 		echo "table already exists ! " ;
 	else
@@ -39,35 +44,40 @@ function createTable {
 			case $choice in
 				*)
 				primaryKey=${columnNames[$REPLY]}
-				echo "reply variable is $REPLY" ;
 				break;;
 			esac
 		done
-		touch tableinfo/$tableName ;
-		touch tabledata/$tableName ;
-		echo "primarykey:$primaryKey" > tableinfo/$tableName ;
+		touch $1/tableinfo/$tableName ;
+		touch $1/tabledata/$tableName ;
+		echo "Creating Table..."
+		sleep 3;
+		echo "primarykey:$primaryKey" > $1/tableinfo/$tableName ;
 
 		for i in `seq 1 $numberOfColumns`
 		do
-			echo "${columnNames[i]}:${columnType[i]}" >> tableinfo/$tableName ;
+			echo "${columnNames[i]}:${columnType[i]}" >> $1/tableinfo/$tableName ;
 		done
 
 		echo "Table Created Successfully ";
 		echo "Table name is $tableName" ;
 		echo "Table Content is " ;
-		echo `cat tableinfo/$tableName`;
+		echo `cat $1/tableinfo/$tableName`;
 		echo "Primary Key is $primaryKey" ;
 	fi
 }
+
+##################################################
+# Functions that handles inserting data
+##################################################
 function insertData {
 	#echo "function insert data , parameter passed is $1 " ;
-	let noOfColumns=`cat tableinfo/$1 | wc -l`;
-	primaryKey=`cat tableinfo/$1 | cut -d: -f2 | cut -d$'\n' -f1` ;
+	let noOfColumns=`cat $2/tableinfo/$1 | wc -l`;
+	primaryKey=`cat $2/tableinfo/$1 | cut -d: -f2 | cut -d$'\n' -f1` ;
 	#echo "$primaryKey is primary key " ;
 	for i in `seq 2 $noOfColumns`
 	do
-		columnsNames[$i]=`cat tableinfo/$1 | cut -d: -f1 | cut -d$'\n' -f$i` ;
-		columnsTypes[$i]=`cat tableinfo/$1 | cut -d: -f2 | cut -d$'\n' -f$i` ;
+		columnsNames[$i]=`cat $2/tableinfo/$1 | cut -d: -f1 | cut -d$'\n' -f$i` ;
+		columnsTypes[$i]=`cat $2/tableinfo/$1 | cut -d: -f2 | cut -d$'\n' -f$i` ;
 	done
 	echo ${columnsNames[@]};
 	for i in `seq 2 $noOfColumns`
@@ -94,13 +104,13 @@ function insertData {
 		if [ ${columnsNames[i]} == $primaryKey ]
 		then
 			#echo "this is the primary key  validation ";
-			commandResult=`awk -v val="$primaryKeyValue" '{ if( $1 == val) print "true"  ; }' tabledata/$1`;
+			commandResult=`awk -v val="$primaryKeyValue" '{ if( $1 == val) print "true"  ; }' $2/tabledata/$1`;
 			while [ $commandResult ]
 			do
 				echo "WARNING!! Primary Key Duplication, Please try another value...";
 				read  inputs[$i] ;
 				primaryKeyValue=${inputs[i]} ;
-				commandResult=`awk -v val="$primaryKeyValue" '{ if( $1 == val) print "true"  ; }' tabledata/$1`;
+				commandResult=`awk -v val="$primaryKeyValue" '{ if( $1 == val) print "true"  ; }' $2/tabledata/$1`;
 				echo $commandResult ;
 			done
 			#echo " Primary key accepted ";
@@ -126,7 +136,7 @@ function insertData {
 	# Writing into Table File
 	##############################################
 	outputString=${inputs[@]} ;
-	echo $primaryKeyValue $outputString >> tabledata/$1 ;
+	echo $primaryKeyValue $outputString >> $2/tabledata/$1 ;
 
 	echo "Record was inserted Successfully as :"
 	echo $outputString ;
@@ -135,11 +145,11 @@ function insertData {
 }
 function insert {
 	echo "Select Table to insert into " ;
-	noOfTables=`ls  tableinfo | wc -w ` ;
+	noOfTables=`ls  $1/tableinfo | wc -w ` ;
 	# Getting the existing tables into an array
 	for i in `seq 1 $noOfTables`
 	do
-		tables[$i]=`ls tableinfo | tr " " "\n" | tr "\n" ":" | cut -f$i -d:` ;
+		tables[$i]=`ls $1/tableinfo | tr " " "\n" | tr "\n" ":" | cut -f$i -d:` ;
 		#echo "table element number $i is ${tables[i]}"
 	done
 
@@ -149,7 +159,7 @@ function insert {
 	case $REPLY in
 	[`seq 1 $noOfTables`] )
 			#echo " you entered $choice ";
-			insertData $choice ;
+			insertData $choice $1;
 			echo "Press Any Key to continue.." ;
 		break;;
 		'Back' )
@@ -162,16 +172,20 @@ function insert {
 	done
 
 }
+
+##################################################
+# Functions that handles deleting data
+##################################################
 function deleteTable {
 	echo "Select Table to Delete " ;
-	noOfTables=`ls  tableinfo | wc -w ` ;
+	noOfTables=`ls  $1/tableinfo | wc -w ` ;
 
 	##############################################
 	# Getting the existing tables into an array
 	##############################################
 	for i in `seq 1 $noOfTables`
 	do
-		tables[$i]=`ls tableinfo | tr " " "\n" | tr "\n" ":" | cut -f$i -d:` ;
+		tables[$i]=`ls $1/tableinfo | tr " " "\n" | tr "\n" ":" | cut -f$i -d:` ;
 	done
 	##############################################
 
@@ -186,7 +200,7 @@ function deleteTable {
 			read YorN ;
 			case $YorN in
 				[yY]* )
-					rm tableinfo/$choice tabledata/$choice ;
+					rm $1/tableinfo/$choice $1/tabledata/$choice ;
 					echo "Table Deleted Successfully ";
 					break;;
 				* )
@@ -205,14 +219,14 @@ function deleteTable {
 }
 function deleteRecord {
 	echo "Select Table to Delete From " ;
-	noOfTables=`ls  tableinfo | wc -w ` ;
+	noOfTables=`ls  $1/tableinfo | wc -w ` ;
 
 	##############################################
 	# Getting the existing tables into an array
 	##############################################
 	for i in `seq 1 $noOfTables`
 	do
-		tables[$i]=`ls tableinfo | tr " " "\n" | tr "\n" ":" | cut -f$i -d:` ;
+		tables[$i]=`ls $1/tableinfo | tr " " "\n" | tr "\n" ":" | cut -f$i -d:` ;
 	done
 	##############################################
 
@@ -229,7 +243,7 @@ function deleteRecord {
 
 			echo " Please Enter The Primary Key for the Record ";
 			read PKey ;
-			commandResult=`awk -v val="$PKey" '{ if( $1 == val) print "true"  ; }' tabledata/$choice`;
+			commandResult=`awk -v val="$PKey" '{ if( $1 == val) print "true"  ; }' $1/tabledata/$choice`;
 			if [ $commandResult ]
 			then
 				echo "Record Found, Are you sure you want to delete the item ?" ;
@@ -238,7 +252,7 @@ function deleteRecord {
 					[yY]* )
 						echo "Deleting Record..." ;
 						sleep 2;
-						deleteCommand=`awk -v val="$PKey" '{ if( $1 != val) print $0 ; }' tabledata/$choice >> tabledata/${choice}.tmp | sleep 2 | mv tabledata/${choice}.tmp tabledata/${choice}`;
+						deleteCommand=`awk -v val="$PKey" '{ if( $1 != val) print $0 ; }' $1/tabledata/$choice >> $1/tabledata/${choice}.tmp | sleep 2 | mv $1/tabledata/${choice}.tmp $1/tabledata/${choice}`;
 						break ;
 					;;
 					[nN]* )
@@ -279,11 +293,11 @@ function deleteChoices {
 		case $del in
 			'Delete Table' )
 					clear;
-					deleteTable ;
+					deleteTable $1 ;
 					break;;
 			'Delete Record' )
 					clear;
-					deleteRecord ;
+					deleteRecord $1 ;
 
 					break;;
 			'Back' )
@@ -295,36 +309,38 @@ function deleteChoices {
 		esac
 	done
 }
+
+##################################################
+# Functions that handles displaying information
+##################################################
 function displayFullTable {
+	clear ;
 	echo "Select Table to View From :" ;
-	noOfTables=`ls  tableinfo | wc -w ` ;
+	noOfTables=`ls  $1/tableinfo | wc -w ` ;
+	let backBtn=noOfTables+1 ;
 	# Getting the existing tables into an array
 	for i in `seq 1 $noOfTables`
 	do
-		tables[$i]=`ls tableinfo | tr " " "\n" | tr "\n" ":" | cut -f$i -d:` ;
-		#echo "table element number $i is ${tables[i]}"
+		tables[$i]=`ls $1/tableinfo | tr " " "\n" | tr "\n" ":" | cut -f$i -d:` ;
 	done
-	echo "number of tables is $noOfTables" ;
 	select choice in ${tables[@]} 'Back'
 	do
 	case $REPLY in
 	[`seq 1 $noOfTables`] )
 
 			echo "Loading Table...";
-			cat tableinfo/$choice | cut -f1 -d: | tr '\n' ' ' | cat > tbl.tmp  ;
+			cat $1/tableinfo/$choice | cut -f1 -d: | tr '\n' ' ' | cat > tbl.tmp  ;
 			echo " " >> tbl.tmp;
-			cat tabledata/$choice | column -t | cat >> tbl.tmp;
+			cat $1/tabledata/$choice | column -t | cat >> tbl.tmp;
 
-			sleep 3 ;
+			sleep 1 ;
 			clear ;
 			cat tbl.tmp | column -t
 			sleep 1 ;
 			rm tbl.tmp ;
-			echo " " ;
-
-			echo "Press Any Key to continue.." ;
+			sleep 1 ;
 			break;;
-		'Back' )
+			"$backBtn" )
 				return 0 ;
 				;;
 		* )
@@ -334,15 +350,16 @@ function displayFullTable {
 	done
 }
 function displayPartTable {
-	echo "display part table func" ;
+	clear;
 	echo "Select Table to View From: " ;
-	noOfTables=`ls  tableinfo | wc -w ` ;
+	noOfTables=`ls  $1/tableinfo | wc -w ` ;
+	let backBtn=$noOfTables+1;
 	#################################################
 	# Getting the existing tables into an array
 	#################################################
 	for i in `seq 1 $noOfTables`
 	do
-		tables[$i]=`ls tableinfo | tr " " "\n" | tr "\n" ":" | cut -f$i -d:` ;
+		tables[$i]=`ls $1/tableinfo | tr " " "\n" | tr "\n" ":" | cut -f$i -d:` ;
 	done
 	#################################################
 	select choice in ${tables[@]} 'Back'
@@ -365,9 +382,9 @@ function displayPartTable {
 									echo "Please Enter the number of lines you want to view ";
 									read noOfLines;
 									echo "Loading Info...";
-									cat tableinfo/$choice | cut -f1 -d: | tr '\n' ' ' | cat > tbl.tmp  ;
+									cat $1/tableinfo/$choice | cut -f1 -d: | tr '\n' ' ' | cat > tbl.tmp  ;
 									echo " " >> tbl.tmp;
-									cat tabledata/$choice | column -t | cat >> tbl.tmp;
+									cat $1/tabledata/$choice | column -t | cat >> tbl.tmp;
 
 									sleep 3 ;
 									clear ;
@@ -375,15 +392,14 @@ function displayPartTable {
 									sleep 1 ;
 									rm tbl.tmp ;
 									echo " " ;
-									#head -$noOfLines
 									break ;;
 								'View Tail' )
 									echo "Please Enter the number of lines you want to view ";
 									read noOfLines;
 									echo "Loading Info...";
-									cat tableinfo/$choice | cut -f1 -d: | tr '\n' ' ' | cat > tbl.tmp  ;
+									cat $1/tableinfo/$choice | cut -f1 -d: | tr '\n' ' ' | cat > tbl.tmp  ;
 									echo " " >> tbl.tmp;
-									cat tabledata/$choice | column -t | cat >> tbl.tmp;
+									cat $1/tabledata/$choice | column -t | cat >> tbl.tmp;
 
 									sleep 3 ;
 									clear ;
@@ -391,7 +407,7 @@ function displayPartTable {
 									sleep 1 ;
 									rm tbl.tmp ;
 									echo " " ;
-									#head -$noOfLines
+
 									break ;;
 								'Back' )
 									break;
@@ -411,7 +427,7 @@ function displayPartTable {
 						clear;
 						echo "Please enter Primary key for the record ";
 						read primaryKeyValue ;
-						commandResult=`awk -v val="$primaryKeyValue" '{ if( $1 == val) print "true"  ; }' tabledata/$choice`;
+						commandResult=`awk -v val="$primaryKeyValue" '{ if( $1 == val) print "true"  ; }' $1/tabledata/$choice`;
 						until [ $commandResult ]
 						do
 							echo "Primary Key Doesn't exist , Please try Again OR Type EXIT to Abort!";
@@ -420,11 +436,10 @@ function displayPartTable {
 							then
 								break 2 ;
 							else
-								commandResult=`awk -v val="$primaryKeyValue" '{ if( $1 == val) print "true"  ; }' tabledata/$choice`;
-								#echo $commandResult ;
+								commandResult=`awk -v val="$primaryKeyValue" '{ if( $1 == val) print "true"  ; }' $1/tabledata/$choice`;
 							fi
 						done
-						commandResult=`awk -v val="$primaryKeyValue" '{ if( $1 == val) print $0  ; }' tabledata/$choice  `;
+						commandResult=`awk -v val="$primaryKeyValue" '{ if( $1 == val) print $0  ; }' $1/tabledata/$choice  `;
 						echo "The Record Data is :"
 						echo $commandResult ;
 					break;;
@@ -439,9 +454,8 @@ function displayPartTable {
 				esac
 
 			done
-			echo "Press Any Key to continue.." ;
-		break;;
-		'Back' )
+			break;;
+		"$backBtn" )
 			clear
 			return 0 ;
 			;;
@@ -452,65 +466,231 @@ function displayPartTable {
 	done
 
 }
-function display {
+function displayTables {
 	clear;
 	select choice in 'Display Full Table ' 'Display Part of Table' 'Back'
 	do
 		case $choice in
 			'Display Full Table ' )
-				displayFullTable ;
-				read
-				return 0
+				displayFullTable $1 ;
+				break;
 			;;
 			'Display Part of Table' )
-				displayPartTable ;
-				read
-				return 0
+				displayPartTable $1 ;
+				break;
 			;;
 			'Back' )
-				return 0 ;
+				break ;
 				;;
 		esac
 
 	done
 }
-while true
-	do
-		clear ;
-	select choice in 'Create Table' 'Insert New Record' 'Display' 'Delete' 'Exit'
-	do
 
-	case $choice in
-	'Create Table' )
+##################################################
+# Functions that handles manipulating databases
+##################################################
+function operationslist {
+
+	while true
+	do
 		clear;
-		createTable ;
-		break;
-	;;
-	'Insert New Record' )
-		clear;
-		insert ;
-		break;
-		#echo "Press any key to continue..." ;
-	;;
-	'Display' )
-		clear;
-		display ;
-		break;
-	;;
-	'Delete' )
-		clear;
-		deleteChoices ;
-		echo "Record Deleted Successfully ";
-		echo "Press any key to continue..." ;
-		break;
-	;;
-	'Exit' )
-		echo "Bye!"
-		exit 0 ;
-	;;
-	* )
-		echo "invalid input! Please try again "
-	;;
-	esac
+		select choice in 'Create Table' 'Insert New Record' 'Display' 'Delete' 'Back'
+		do
+			case $choice in
+			'Create Table' )
+				clear;
+				createTable $1 ;
+				echo "Press any key to continue..." ;
+				read
+				break;
+			;;
+			'Insert New Record' )
+				clear;
+				insert $1;
+				echo "Press any key to continue..." ;
+				read
+				break;
+				#echo "Press any key to continue..." ;
+			;;
+			'Display' )
+				clear;
+				displayTables $1 ;
+				echo "Press any key to continue..." ;
+				read
+				break;
+			;;
+			'Delete' )
+				clear;
+				deleteChoices $1 ;
+				echo "Record Deleted Successfully ";
+				echo "Press any key to continue..." ;
+				read
+				break;
+			;;
+			'Back' )
+				return 0 ;
+			;;
+			* )
+				echo "invalid input! Please try again "
+			;;
+			esac
+		done
 	done
-done
+}
+
+##################################################
+# Functions that handles databases
+##################################################
+function displayDatabases {
+	clear;
+	echo "Please Selected the Database to use :"
+	#################################################
+	# Getting the existing databases into an array
+	#################################################
+	unset 'databases' ;
+	noOfDatabases=`ls -l | grep ^d | cut -d" " -f10 | wc -l ` ;
+	let backBtn=$noOfDatabases+1 ;
+	# Getting the existing tables into an array
+	for i in `seq 1 $noOfDatabases`
+	do
+		databases[$i]=`ls -l | grep ^d | cut -d" " -f10 | grep .db$ | tr "\n" " " |  cut -f$i -d" " ` ;
+	done
+	#	echo ${databases[@]};
+	select db in ${databases[@]} 'Back'
+	do
+		case $REPLY in
+			[`seq 1 $noOfDatabases`] )
+				clear;
+				echo " Database $db Selected ";
+				operationslist $db ;
+				break ;
+				;;
+				"$backBtn" )
+					clear;
+					return 0 ;
+					;;
+				* )
+					echo "Invalid input, Please try again!"
+				;;
+		esac
+	done
+}
+function createDatabase {
+	clear;
+	echo "Please Enter Database name : ";
+	read dbName ;
+	if [ -d $dbName ]
+	then
+		echo "Database already exists ! " ;
+	else
+		clear ;
+		echo "Creating Database..." ;
+		mkdir $dbName.db ;
+		sleep 2 ;
+		echo "Initializing Schema..."
+		mkdir $dbName.db/tableinfo $dbName.db/tabledata ;
+		sleep 3 ;
+		echo "Database Created Successfully ";
+		return 0 ;
+	fi
+}
+function deleteDatabase {
+	#################################################
+	# Getting the existing databases into an array
+	#################################################
+	clear ;
+	echo "select a Database to delete " ;
+	noOfDatabases=`ls -l | grep ^d | cut -d" " -f10 | wc -l ` ;
+	let backBtn=$noOfDatabases+1 ;
+	# Getting the existing tables into an array
+	for i in `seq 1 $noOfDatabases`
+	do
+		databases[$i]=`ls -l | grep ^d | cut -d" " -f10 | grep .db$ | tr "\n" " " |  cut -f$i -d" " ` ;
+	done
+	#	echo ${databases[@]};
+	select db in ${databases[@]} 'Back'
+	do
+		case $REPLY in
+			[`seq 1 $noOfDatabases`] )
+				clear;
+				echo " Database $db Selected ";
+				echo "Are you sure you want to delete $db ?!! " ;
+				read YorN ;
+				case $YorN in
+					[yY]* )
+						echo "Deleting Database $db ... ";
+						rm -R $db ;
+						sleep 2;
+						echo "Database Removed Successfully ";
+						break ;
+						;;
+						[nN]* )
+							echo "Aborting..."
+							return 0 ;
+						break;
+						;;
+						* )
+							echo "Invalid Input, Aborting...";
+						break;
+						;;
+					esac
+				# if [ $YorN = [yY]* ]
+				# then
+				# 	echo "Deleting Database $db ... ";
+				# 	rm -R $db ;
+				# 	sleep 2;
+				# 	echo "Database Removed Successfully ";
+				# elif [ $YorN = [nN]* ]
+				# then
+				# 	echo "Aborting..."
+				# 	sleep 2 ;
+				# 	return 0 ;
+				# else
+				# 	echo "Invalid input,Aborting !!!" ;
+				# fi
+				break ;
+				;;
+				"$backBtn" )
+					clear;
+					return 0 ;
+					;;
+				* )
+					echo "Invalid input, Please try again!"
+				;;
+		esac
+	done
+}
+##################################################
+
+	clear;
+	select choice in 'Use Database' 'Create Database' 'Delete Database' 'Exit'
+	do
+		case $choice in
+			'Use Database' )
+				clear;
+				displayDatabases ;
+				echo "Press any key to contine.."
+
+				;;
+			'Create Database' )
+				clear;
+				createDatabase ;
+				echo "Press any key to contine.."
+
+				;;
+			'Delete Database' )
+				clear;
+				deleteDatabase ;
+				echo "Press any key to contine.."
+
+				;;
+			'Exit' )
+				echo "Bye!";
+				exit 0 ;
+				;;
+				* )
+					echo "Invalid input, Please try again!" ;
+					;;
+			esac
+	done
